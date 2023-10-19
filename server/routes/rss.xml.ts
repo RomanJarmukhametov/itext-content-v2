ca250e26ccb3611690ffc81143da8bb5ca070b60
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2023-10-19 11:12:42 +0600

    Update rss.xml.ts

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -3,18 +3,15 @@ import RSS from 'rss'
 
 
 const feed = new RSS({
-
   title: 'Michael Hoffmann',
-
   site_url: 'https://itext.agency',
-
   feed_url: `https://itext.agency/rss.xml`,
-
 })
 
 export default defineEventHandler(async (event) => {
   const docs = await serverQueryContent(event).sort({ date: -1 }).find()
-    const blogPosts = docs.filter((doc) => doc?._path?.includes('/en/blog'))
+  const blogPosts = docs.filter((doc) => doc?._path?.includes('/en/blog'))
+    
     
 for (const doc of blogPosts) {
   feed.item({
@@ -22,8 +19,13 @@ for (const doc of blogPosts) {
     url: `https://itext.agency${doc._path}`,
     date: doc.date,
     description: doc.description,
+    enclosure: {
+      url: `https://itext.agency/images/blog/${doc.image}`,
+      type: 'image/jpeg' // Assuming it's a JPEG image; modify as needed
+    }
   })
 }
+
 
   const feedString = feed.xml({ indent: true })
   event.res.setHeader('content-type', 'text/xml')
